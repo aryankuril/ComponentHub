d9b5806360831225ea70abe09ec35c6eed9c1c60
Aryan Kuril <aryan@bombayblokes.com>
2025-09-02 15:18:13 +0530

    okok push

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+// next.config.js
+module.exports = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
-
-module.exports = nextConfig;
