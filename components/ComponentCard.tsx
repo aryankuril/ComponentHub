deaeef0c2a0293ff5cb40ead4aad558f6d971098
Aryan Kuril <aryan@bombayblokes.com>
2025-09-11 11:40:42 +0530

    yes push

--- components/ComponentCard.tsx
+++ components/ComponentCard.tsx
@@ -34,6 +34,7 @@ export default function ComponentCard({ component, onPreview, onCopy }: Componen
             <div className="w-16 h-16 bg-[#F9B31B] rounded-lg animate-pulse" />
           </div>
         )}
+        {/*  */}
 
         {/* Overlay */}
         <div className="absolute inset-0 bg-gradient-to-t from-black/60 via-transparent to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
